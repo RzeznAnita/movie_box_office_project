a3b216f58d4a3e56e856051e7340786ea3be01c4
Anita Rzeznicka <rzeznicka.anita@gmail.com>
2025-08-03 15:52:13 +0200

    Adding Power BI Dashboard and ER Diagram

--- src/run_etl.py
+++ src/run_etl.py
@@ -2,6 +2,7 @@ from etl import ETLPipeline
 
 if __name__ == "__main__":
     """Run ETL."""
-    API_KEY = "e4cc9277"
+    API_KEY = "YOUR_OMDB_API_KEY
+    
     etl = ETLPipeline(omdb_api_key=API_KEY)
     etl.run()
